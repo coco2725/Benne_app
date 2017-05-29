dce21fa67745badec5e1afd7f43af73029f38800
coucou1121 <sebastien.blessemaille@heig-vd.ch>
2017-05-29 19:23:29 +0200

    Class ouvrier
    Add fonction et tourne en boucle

--- Ouvrier.java
+++ Ouvrier.java
@@ -1,6 +1,10 @@
+import java.util.concurrent.TimeUnit;
 
 public class Ouvrier extends Thread 
 {
+	private int state = 0;
+	private int maxTime = 10;
+
 	public Ouvrier(String name)
 	{
 		super(name);
@@ -8,26 +12,72 @@ public class Ouvrier extends Thread
 	
 	public void run()
 	{
-			System.out.println(this.getName());
+		System.out.println(this.getName() + " creer");
+		while(true)
+		{
+			switch (state){
+			case 0: dechargeBenne();
+			break;	
+			case 1: ammenerUsine();
+			break;	
+			case 2: scieBois();
+			break;
+			case 3: retourneBenne();
+			break;
+			default : break;
+			}
+		} 
 	} 
 	
-	public void coupe()
+	public void dechargeBenne()
 	{
+		System.out.println(this.getName() + " decharge la benne");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public void ammenerBenne()
+	public void ammenerUsine()
 	{
+		System.out.println(this.getName() + " ammene a l'usine");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public void decharge()
+	public void scieBois()
 	{
+		System.out.println(this.getName() + " scie le bois");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public void retourneForet()
+	public void retourneBenne()
 	{
+		System.out.println(this.getName() + " retourne a la benne");
+		state=0;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
