c36f6d11df7b76d622cf8c7feae732f4fbc3025a
coucou1121 <sebastien.blessemaille@heig-vd.ch>
2017-05-29 20:34:11 +0200

    classe benne
    Add fonctions mais pas fini :-)

--- Benne.java
+++ Benne.java
@@ -1,4 +1,87 @@
+import java.util.concurrent.TimeUnit;
 
 public class Benne {
+
+	private int state = 0;
+	private int maxTime = 10;
+
+	public Bucheron(String name)
+	{
+		super(name);
+	}
+
+	public void run()
+	{
+		System.out.println(this.getName() + "creer");
+		while(true)
+		{
+			switch (state){
+			case 0: remplire();
+			break;	
+			case 1: ammarerForet();
+			break;	
+			case 2: transportDeForetVersUsine();
+			break;
+			case 3: desamreUsine();
+			break;
+			case 4: amarerUsine();
+			break;
+			case 5: transportDeUsineAForet();
+			break;
+			case 6: desammarerForet();
+			break;
+			default : break;
+			}
+		} 
+	}
+	
+	public void remplire()
+	{
+		System.out.println(this.getName() + " remplie");
+		state++;
+	}
+
+	public void ammarerForet()
+	{
+		System.out.println(this.getName() + " est ammarée");
+		state++;
+	}
+
+	public void transportDeForetVersUsine()
+	{
+		System.out.println(this.getName() + " transport de foret vers usine");
+		state++;
+	}
+
+	public void desamreUsine()
+	{
+		System.out.println(this.getName() + " desammarer à l'usine");
+		state=++;
+	}
+	
+	public void vider()
+	{
+		System.out.println(this.getName() + " est vidée");
+		state=++;
+	}
+
+	
+	public void amarerUsine()
+	{
+		System.out.println(this.getName() + " ammarer à l'usine");
+		state=++;
+	}
+
+	public void transportDeUsineAForet()
+	{
+		System.out.println(this.getName() + " ammarer à l'usine");
+		state=++;
+	}
+
+	public void desammarerForet()
+	{
+		System.out.println(this.getName() + " ammarer à l'usine");
+		state=0;
+	}
 
 }
--- Transporteur.java
+++ Transporteur.java
@@ -1,6 +1,10 @@
+import java.util.concurrent.TimeUnit;
 
 public class Transporteur extends Thread 
 {
+	private int state = 0;
+	private int maxTime = 10;
+
 	public Transporteur(String name)
 	{
 		super(name);
@@ -8,39 +12,112 @@ public class Transporteur extends Thread
 
 	public void run()
 	{
-		System.out.println(this.getName());
+		System.out.println(this.getName() + " creer");
+		while(true)
+		{
+			switch (state){
+			case 0: transporteBenneDeUsineAForet();
+			break;	
+			case 1: desamarreBenneForet();
+			break;	
+			case 2: amarreBenneForet();
+			break;
+			case 3: transporteBenneDeForetAUsine();
+			break;
+			case 4: desamarreBenneUsine();
+			break;
+			case 5: amarreBenneUsine();
+			break;
+
+			default : break;
+			}
+		} 
 	} 
 
 
 
 	public void transporteBenneDeUsineAForet()
 	{
+		System.out.println(this.getName() + " transporte la benne usine -> foret");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
 
 	public void desamarreBenneForet()
 	{
+		System.out.println(this.getName() + " desamarre benne à la foret");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
 
 	public void amarreBenneForet()
 	{
+		System.out.println(this.getName() + " amarre benne à la foret");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
 
 	public void transporteBenneDeForetAUsine()
 	{
+		System.out.println(this.getName() + " transporte la benne foret -> usine");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
 
 	public void desamarreBenneUsine()
 	{
+		System.out.println(this.getName() + " desamarre benne à l'usine");
+		state++;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
 
 
 	public void amarreBenneUsine()
 	{
+		System.out.println(this.getName() + " amarre benne à l'usine");
+		state=0;
+		try {
+			TimeUnit.SECONDS.sleep((int)(1+Math.random()*maxTime));
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
 
