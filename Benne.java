95fac3bd4d989094475f22ed1dbb6e4155381ce0
coucou1121 <sebastien.blessemaille@heig-vd.ch>
2017-06-04 14:53:59 +0200

    ajout getName, getEtat, set Etat

--- Benne.java
+++ Benne.java
@@ -1,75 +1,73 @@
-import java.util.concurrent.TimeUnit;
-
 public class Benne {
 
 	private int state = 0;
-	private int maxTime = 10;
+	private int _numBenne = 0;
 	private String _name = "unknow";
 
-	public Benne(String name)
+	public Benne(String name, int numBenne)
 	{
 		this._name = name;
+		this._numBenne = numBenne;
 		this.state = 0;
 	}
 
-	public void run()
+	public void getEtat()
 	{
-		System.out.println(this.getName() + "creer");
-		while(true)
-		{
-			switch (state){
-			case 0: remplire();
-			break;	
-			case 1: ammarerForet();
-			break;	
-			case 2: transportDeForetVersUsine();
-			break;
-			case 3: desamreUsine();
-			break;
-			case 4: amarerUsine();
-			break;
-			case 5: transportDeUsineAForet();
-			break;
-			case 6: desammarerForet();
-			break;
-			default : break;
-			}
-		} 
+		switch (state){
+		case 0: remplire();
+		break;	
+		case 1: ammarerForet();
+		break;	
+		case 2: transportDeForetVersUsine();
+		break;
+		case 3: desamreUsine();
+		break;
+		case 4: amarerUsine();
+		break;
+		case 5: transportDeUsineAForet();
+		break;
+		case 6: desammarerForet();
+		break;
+		default : break;
+		}
 	}
-	
+
 	public String getName()
 	{
 		return this._name;
 	}
+	
+	public void setEtat(int etat)
+	{
+		this.state = etat;
+	}
+	
 	public void remplire()
 	{
 		System.out.println(this.getName() + " remplie");
-		state++;
 	}
 
 	public void ammarerForet()
 	{
 		System.out.println(this.getName() + " est ammarée");
-		state++;
 	}
 
 	public void transportDeForetVersUsine()
 	{
 		System.out.println(this.getName() + " transport de foret vers usine");
-		state++;
 	}
 
 	public void desamreUsine()
 	{
 		System.out.println(this.getName() + " desammarer à l'usine");
 	}
-	
+
 	public void vider()
 	{
 		System.out.println(this.getName() + " est vidée");
 	}
 
-	
+
 	public void amarerUsine()
 	{
 		System.out.println(this.getName() + " ammarer à l'usine");
--- Benne_application.java
+++ Benne_application.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //import thread_01BASE_THREAD.Personne;
 
 public class Benne_application {
@@ -6,6 +8,31 @@ public class Benne_application {
 	{
 		// TODO Auto-generated method stub
 
+		int NbBenne = 3;
+		
+		/*
+		 * au départ: toutes les bennes sont vide et dans le parque
+		 * Le transporteur peux ammarer la première
+		 * Le bucheron peut aller couper du bois.
+		 * L'ouvrier attends l'arrivée de la première benne
+		 * 
+		 */
+		
+		//création de la liste de benne
+		ArrayList<Benne> listDeBennes = new ArrayList<Benne>();
+		
+		for(int i = 1; i <= NbBenne ; i++)
+		{
+			Benne benne = new Benne("Benne" + i, i);
+			listDeBennes.add(benne);
+		}
+		
+		for(int i = 0; i < listDeBennes.size() ; i++)
+		{
+			System.out.println("nom de benne " + i + " : " + listDeBennes.get(i).getName());
+		}
+		
+		
 		Transporteur T1 = new Transporteur("Transporteur 1");
 		Ouvrier O1 = new Ouvrier("Ouvrier 1");
 		Bucheron B1 = new Bucheron("Bucheron 1");
